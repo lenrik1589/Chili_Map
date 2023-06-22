7d576cafe4f66a689fa6a9c8b958dd1d78c660e9
Alice <alice.is.cute@proton.me>
2023-06-22 09:35:49 +0300

    update deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,24 +2,24 @@ import org.gradle.jvm.tasks.Jar
 import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
 
 plugins {
-	id ("org.ajoberstar.grgit") version "4.1.0"
 	id ("maven-publish")
-	id ("org.quiltmc.loom") version "1.0.+"
-	id ("org.jetbrains.kotlin.jvm") version "1.8.10"	
+	id ("org.ajoberstar.grgit") version "4.1.0"
+	id ("org.jetbrains.kotlin.jvm") version "1.8.10"
+	id ("org.quiltmc.loom") version "1.2.+"
 }
 
 version = project.properties["mod_version"]!!
 group = project.properties["maven_group"]!!
-val minecraft = project.properties["minecraft"]
-val quilt_mappings = project.properties["quilt_mappings"]
-val quilt_loader = project.properties["quilt_loader"]
-val qfapi = project.properties["qfapi"]
-val qlk = project.properties["qlk"]
-val carpet = project.properties["carpet"]
-val malilib = project.properties["malilib"]
-val minihud = project.properties["minihud"]
-val mod_menu = project.properties["mod_menu"]
-val placeholders = project.properties["placeholders"]
+val minecraft by project.properties
+val quilt_mappings by project.properties
+val quilt_loader by project.properties
+val qfapi by project.properties
+val qlk by project.properties
+val carpet by project.properties
+val malilib by project.properties
+val minihud by project.properties
+val mod_menu by project.properties
+val placeholders by project.properties
 
 quiltflower {
 	addToRuntimeClasspath.set(true)
@@ -29,14 +29,6 @@ repositories {
 	maven {
 		name = "Modrinth"
 		url = uri("https://api.modrinth.com/maven")
-	}
-	maven {
-		name = "Terraformers"
-		url = uri("https://maven.terraformersmc.com/releases/")
-	}
-	maven {
-		name = "Nucleoid's"
-		url = uri("https://maven.nucleoid.xyz/")
 	}
 	flatDir {
 		dirs = setOf(file("libs"))
@@ -50,22 +42,23 @@ repositories {
 }
 
 dependencies {
-	 minecraft           ("com.mojang:minecraft:$minecraft")
-	 mappings            ("org.quiltmc:quilt-mappings:$minecraft+build.$quilt_mappings:intermediary-v2")
-	 modImplementation   ("org.quiltmc:quilt-loader:$quilt_loader")
-	 modImplementation   ("org.quiltmc.quilted-fabric-api:quilted-fabric-api:$qfapi-$minecraft")
-	 modImplementation   ("org.quiltmc.quilt-kotlin-libraries:quilt-kotlin-libraries:$qlk") {
-		 exclude(group = "org.quiltmc", module = "qsl" )
-	 }
-	 implementation      ("com.github.LlamaLad7:MixinExtras:0.1.1")
-	 annotationProcessor ("com.github.LlamaLad7:MixinExtras:0.1.1")
-	 modImplementation   ("com.github.gnembon:fabric-carpet:$carpet") { isTransitive = false }
-	 modImplementation   ("fi.dy.masa:malilib-fabric:$minecraft-$malilib") { isTransitive = false }
-	 modImplementation   ("fi.dy.masa:minihud-fabric:$minecraft-$minihud") { isTransitive = false }
-	 modImplementation   ("maven.modrinth:modmenu:$mod_menu") { isTransitive = false }
-	 modImplementation   ("eu.pb4:placeholder-api:$placeholders") { isTransitive = false; include(this) }
-	 modCompileOnly      ("essential-client:essential-client:+") { isTransitive = false }
-	 modCompileOnly      ("chunkdebug:chunk-debug:+")
+	minecraft           ("com.mojang:minecraft:$minecraft")
+	mappings            ("org.quiltmc:quilt-mappings:$minecraft+build.$quilt_mappings:intermediary-v2")
+	modImplementation   ("org.quiltmc:quilt-loader:$quilt_loader")
+	modImplementation   ("org.quiltmc.quilted-fabric-api:quilted-fabric-api:$qfapi-$minecraft")
+	modImplementation   ("org.quiltmc.quilt-kotlin-libraries:quilt-kotlin-libraries:$qlk") {
+		exclude(group = "org.quiltmc", module = "qsl" )
+	}
+	implementation      ("com.github.LlamaLad7:MixinExtras:0.1.1")
+	annotationProcessor ("com.github.LlamaLad7:MixinExtras:0.1.1")
+	modImplementation   ("com.github.gnembon:fabric-carpet:$carpet") { isTransitive = false }
+	modImplementation   ("fi.dy.masa:malilib-fabric:$minecraft-$malilib") { isTransitive = false }
+	modImplementation   ("fi.dy.masa:minihud-fabric:$minecraft-$minihud") { isTransitive = false }
+	modImplementation   ("maven.modrinth:modmenu:$mod_menu") { isTransitive = false }
+	modImplementation   ("maven.modrinth:placeholder-api:FplcrCus") { include(this) }
+	modImplementation   ("maven.modrinth:essentialclient:vdSDpP5i")
+	implementation      ("com.github.senseiwells:Arucas:+")
+	modImplementation   ("maven.modrinth:chunkdebug:qqJd9qYA")
 
 }
 
